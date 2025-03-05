a406c1ff9ea7d0bb973359ddeecf395de7bdd528
Zeus <dungnguyen10082000@gmail.com>
2025-03-05 15:45:41 +0700

    sum docstring

--- python/app/src/coord_live.py
+++ python/app/src/coord_live.py
@@ -1,3 +1,8 @@
+"""
+Import modules to ensure that the app works
+
+"""
+
 import json
 import os
 import sys
@@ -47,6 +52,12 @@ from PyQt5.QtWidgets import (
 
 
 class ConnectionDialog(QDialog):
+    """
+    Dialog that prompts the user to connect to the ip camera
+
+    Attributes:
+        ip_input: IP Address of the camera
+    """
 
     def __init__(self):
         super().__init__()
@@ -67,6 +78,12 @@ class ConnectionDialog(QDialog):
         self.apply_theme()
 
     def get_ip(self):
+        """
+        Gets the ip address of the video source
+
+        Returns:
+            str: the ip address
+        """
         return self.ip_input.text()
 
     def apply_theme(self):
@@ -89,6 +106,15 @@ class ConnectionDialog(QDialog):
 
 
 class CredentialsDialog(QDialog):
+    """
+    Dialog where the user can enter their username, their password, as well as chose their profile
+
+    Attributes:
+        username_input: username
+        password_input: password, appears as '***' to hide the password
+        profile_combo: the video profile. Profile 1 has the highest quality, 3 has the lowest.
+        Might need to look in to if this is exclusive to Hanwha cameras
+    """
 
     def __init__(self):
         super().__init__()
@@ -118,6 +144,19 @@ class CredentialsDialog(QDialog):
         self.apply_theme()
 
     def get_credentials(self):
+        """
+        Collects the credentials to login the camera, this is put together to
+        form a string that is the url that the program will use to to connect
+        to the camera
+
+        Returns:
+            username_input (str): The username, typically 'admin'
+            password_input (str): The password, visually concealed
+            profile_combo (str): A choice between profile 1, 2, or 3, gradually
+                                decreasing in quality but increasing in
+                                smoothnees
+
+        """
         return (
             self.username_input.text(),
             self.password_input.text(),
@@ -361,6 +400,12 @@ class VideoPointSelector(QWidget):
         self.next_frame()
 
     def next_frame(self):
+        """
+        Display the next frame.
+
+        The method looks for the next frame to show on the screen, draws the pix
+        map on it and shows it on the app
+        """
         if self.video_capture:
             ret, frame = self.video_capture.read()
             if not ret:
@@ -380,6 +425,17 @@ class VideoPointSelector(QWidget):
                 self.draw_polygon([QPointF(x, y) for x, y in self.points])
 
     def set_video_position(self, position):
+        """
+        *Deprecated*: Only really works with static video files, this function
+        allows for scrolling through the video
+
+        This function takes in the position that is selected, compares it to the
+        current position, then if it's different to the new position, sets the
+        new position as the current position.
+
+        Args:
+            position (int):  current position of the playback
+        """
         if self.video:
             self.timer.stop()  # Prevent frame updates while seeking
             self.video.set(cv2.CAP_PROP_POS_FRAMES, position)
@@ -392,6 +448,14 @@ class VideoPointSelector(QWidget):
             self.timer.start(30)  # Resume if playing
 
     def toggle_playback(self):
+        """
+        *Deprecated*: Only really works with static video files, this function
+        allows for play/pause
+
+        This functions toggles the playing state of the app. Essentially,
+        playing is continously showing the next frames, the app has an attribute
+        that monitors this state, which this function can toggle.
+        """
         if self.playing:
             self.timer.stop()  # Stop playback
         else:
@@ -399,6 +463,13 @@ class VideoPointSelector(QWidget):
         self.playing = not self.playing
 
     def display_pixmap(self, pixmap):
+        """
+        Display the dots on top of the frames
+
+        Args:
+            pixmap (Qpixmap): The pixmap that indicates the locations of the
+            dots
+        """
         self.view.viewport().removeEventFilter(self)  # Temporarily disable event filter
         self.current_pixmap = pixmap
 
@@ -423,6 +494,17 @@ class VideoPointSelector(QWidget):
         self.view.viewport().installEventFilter(self)  # Re-enable event filter
 
     def draw_polygon(self, polygon_points):
+        """
+        Draws the polygons from the points
+
+        This function draws a semi-transparent shade highlighting the area
+        convered by the polygon that was made up from the dots. It does not
+        affect the end result other than makes it easier to visualise the shape.
+
+        Args:
+            polygon_points (list of tuples of floats): The coordinates of the
+            dots that needs to be drawn
+        """
         if len(polygon_points) < 2 or not self.toggle_polygon_checkbox.isChecked():
             return  # Not enough points OR checkbox is off
 
@@ -457,6 +539,20 @@ class VideoPointSelector(QWidget):
                 self.scene.addItem(closing_line)
 
     def map_to_image_coordinates(self, pos):
+        """
+        Map the clicks to the correct dot coordinates.
+
+        When the user clicks on a spot, this function translates the coordinate
+        of the cursor at that moment to the pixmap so that the dots are
+        correctly positioned on the live video
+
+        Args:
+            pos (tuple of floats): Current position of the cursor
+
+        Returns:
+            x, y (tuple of floats): Current position of the cursor with regards
+            to the live video (top left is (0, 0))
+        """
         scene_pos = self.view.mapToScene(pos)
         x, y = (
             scene_pos.x() - self.pixmap_item.x(),
@@ -465,6 +561,31 @@ class VideoPointSelector(QWidget):
         return x, y
 
     def eventFilter(self, obj, event):
+        """
+        Filters events for the view's viewport, handling mouse interactions.
+
+        This method processes mouse movements and clicks to update the cursor position
+        and allow the user to add draggable points on an image.
+
+        Args:
+            obj (QObject): The object receiving the event.
+            event (QEvent): The event being processed.
+
+        Returns:
+            bool: True if the event is handled and should not propagate further,
+                  False otherwise.
+
+        Behavior:
+            - Ignores events during video playback or when edit mode is disabled.
+            - On MouseMove:
+                - Updates the cursor position label with the current coordinates.
+            - On MouseButtonPress:
+                - If the left mouse button is clicked within the image boundaries:
+                    - Adds a point if the threshold (max points) is not reached.
+                    - Updates the cursor label with the clicked position.
+                    - Marks the polygon as closed if the threshold is reached.
+
+        """
         threshold = self.threshold_spinbox.value()
         if obj == self.view.viewport() and self.pixmap_item:
             if (
@@ -481,7 +602,7 @@ class VideoPointSelector(QWidget):
 
                 return True  # Stop recursion
 
-            elif event.type() == event.MouseButtonPress:
+            if event.type() == event.MouseButtonPress:
                 x, y = self.map_to_image_coordinates(event.pos())
 
                 if (
@@ -502,11 +623,35 @@ class VideoPointSelector(QWidget):
         return False  # Allow normal event propagation
 
     def draw_point(self, x, y):
+        """
+        Draw a dot on the app
+
+        Args:
+            x (float): x coordinate of the dot
+            y (float): y coordinate of the dot
+        """
         # Draw the red dot
-        dot = self.scene.addEllipse(x - 5, y - 5, 10, 10, QPen(Qt.red), QBrush(Qt.red))
+        dot = self.scene.addEllipse(
+            x - 5,
+            y - 5,
+            10,
+            10,
+            QPen(QColor(237, 135, 150)),
+            QBrush(QColor(237, 135, 150)),
+        )
         dot.setZValue(1)
 
     def undo_last_point(self):
+        """
+        Remove the last drawn point from the scene and update the display.
+
+        This method removes the most recently added point from the `self.points` list
+        and clears all graphical elements except for the video frame. It then redraws
+        the remaining points and, if enabled, re-renders the polygon connecting them.
+
+        Returns:
+            None
+        """
         if not self.points:
             return  # Nothing to undo
 
@@ -533,6 +678,20 @@ class VideoPointSelector(QWidget):
         self.view.setScene(self.scene)
 
     def keyPressEvent(self, event):
+        """
+        Handle key press events for controlling the application.
+
+        This method defines key bindings for various actions:
+        - Spacebar: Toggle video playback (play/pause).
+        - 'Z' key: Undo the last drawn point.
+        - 'Ctrl + S': Save the drawn points as a JSON file.
+
+        Args:
+            event (QKeyEvent): The key press event object.
+
+        Returns:
+            None
+        """
         if event.key() == Qt.Key_Space:
             self.toggle_playback()  # Space: Play/Pause
         elif event.key() == Qt.Key_Z:
@@ -543,6 +702,15 @@ class VideoPointSelector(QWidget):
             super().keyPressEvent(event)  # Pass other events to the default handler
 
     def save_points_json(self):
+        """
+        Save the drawn points to a JSON file.
+
+        This method opens a file dialog to select a save location and writes
+        the list of drawn points to a JSON file.
+
+        Returns:
+            None
+        """
         file_path, _ = QFileDialog.getSaveFileName(
             self, "Save Points as JSON", "", "JSON Files (*.json)"
         )
@@ -551,21 +719,56 @@ class VideoPointSelector(QWidget):
                 json.dump(self.points, f, indent=4)
 
     def close_application(self):
+        """
+        Clean up resources and close the application.
+
+        If a video is open, it is released before closing the application.
+
+        Returns:
+            None
+        """
         if self.video:
             self.video.release()
         self.close()
 
     def wheelEvent(self, event):
+        """
+        Handle mouse wheel events for zooming in and out.
+
+        Scroll up zooms in, while scroll down zooms out.
+
+        Args:
+            event (QWheelEvent): The wheel event object.
+
+        Returns:
+            None
+        """
         if event.angleDelta().y() > 0:
             self.zoom_in()
         else:
             self.zoom_out()
 
     def zoom_in(self):
+        """
+        Zoom in on the view by scaling it up.
+
+        Increases the zoom factor by 20% each time it is called.
+
+        Returns:
+            None
+        """
         self.view.scale(1.2, 1.2)
         self.zoom_factor *= 1.2
 
     def zoom_out(self):
+        """
+        Zoom out on the view by scaling it down.
+
+        Decreases the zoom factor by approximately 16.7% each time it is called.
+
+        Returns:
+            None
+        """
         self.view.scale(1 / 1.2, 1 / 1.2)
         self.zoom_factor /= 1.2
 
